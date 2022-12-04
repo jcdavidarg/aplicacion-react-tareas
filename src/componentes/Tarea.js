6e3c176d8a1214682a407fae20abf8cb269ca52a
jcdavidarg <jcdavidarg@gmail.com>
2022-12-04 02:32:41 -0300

    componente Tarea modificado

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -5,7 +5,9 @@ import { AiOutlineCloseCircle } from "react-icons/ai";
 const Tarea = ({ id, texto, completada, completarTarea, eliminarTarea }) => {
   return (
     <div
-      className={completada ? "tarea-contenedor comletada" : "tarea-contenedor"}
+      className={
+        completada ? "tarea-contenedor completada" : "tarea-contenedor"
+      }
     >
       <div className="tarea-texto" onClick={() => completarTarea(id)}>
         {texto}
