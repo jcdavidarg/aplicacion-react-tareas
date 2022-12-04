8dea5ed32cb2eb16efc658e2df17b410e461db13
jcdavidarg <jcdavidarg@gmail.com>
2022-12-04 01:44:27 -0300

    funcionalidad para componente ListaDeTareas

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -6,9 +6,17 @@ import Tarea from "./Tarea";
 const ListaDeTareas = () => {
   const [tareas, setTareas] = useState([]);
 
+  const agregarTarea = (tarea) => {
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+    }
+  };
+
   return (
     <>
-      <TareaFormulario />
+      <TareaFormulario onSubmit={agregarTarea} />
       <div className="tareas-lista-contenedor">
         {tareas.map((tarea) => (
           <Tarea texto={tarea.texto} completada={tarea.completada} />
