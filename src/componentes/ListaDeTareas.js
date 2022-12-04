aa89cb461d4d4deb3e27e32b8654c45e33f7cdc5
jcdavidarg <jcdavidarg@gmail.com>
2022-12-03 23:17:05 -0300

    map a estate tareas

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../hojas-de-estilo/ListaDeTareas.css";
 import TareaFormulario from "./TareaFormulario";
+import Tarea from "./Tarea";
 
 const ListaDeTareas = () => {
+  const [tareas, setTareas] = useState([]);
+
   return (
     <>
       <TareaFormulario />
-      <div className="tareas-lista-contenedor">LISTA DE TAREAS</div>
+      <div className="tareas-lista-contenedor">
+        {tareas.map((tarea) => (
+          <Tarea texto={tarea.texto} completada={tarea.completada} />
+        ))}
+      </div>
     </>
   );
 };
